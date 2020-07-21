d1df823ac3a21c159f281cdeb69bda7c8c79166f
dychotomy <dychotomy2000@yahoo.com>
2020-07-21 17:12:16 -0400

    Feed Skeleton

---
+++ components/Shared/Feed/Feed.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Feed = () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+});
+
+export default Feed;
