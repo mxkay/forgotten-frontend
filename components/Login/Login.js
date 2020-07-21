785d80042289166fdfeabbaa6d0a3de1c0746e8a
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-07-21 09:49:19 -0400

    added checks against existing/incorrect handles

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 import axios from "axios";
 
 const Login = () => {
   const [input, setInput] = useState("");
   const [user, setUser] = useState({});
   const [newUser, setNewUser] = useState({ name: "", handle: "", email: "" });
-  console.log(newUser);
 
   return (
     <View>
@@ -21,13 +20,13 @@ const Login = () => {
           title="Submit"
           onPress={() => {
             const makeAPICall = async () => {
-              try {
-                const res = await axios(
-                  `https://immense-tor-64805.herokuapp.com/api/user/handle/${input}`
-                );
+              const res = await axios(
+                `https://immense-tor-64805.herokuapp.com/api/user/handle/${input}`
+              );
+              if (res.data.length === 0) {
+                Alert.alert("Invalid handle");
+              } else {
                 setUser(res.data);
-              } catch (err) {
-                console.error(err);
               }
             };
             makeAPICall();
@@ -43,7 +42,9 @@ const Login = () => {
         />
         <TextInput
           placeholder="Handle"
-          onChangeText={(text) => setNewUser({ ...newUser, handle: text })}
+          onChangeText={(text) =>
+            setNewUser({ ...newUser, handle: text.toLowerCase() })
+          }
           value={newUser.handle}
         />
         <TextInput
@@ -56,15 +57,17 @@ const Login = () => {
         title="Submit"
         onPress={() => {
           const makeAPICall = async () => {
-            try {
-              const res = await axios({
+            const res1 = await axios(
+              `https://immense-tor-64805.herokuapp.com/api/user/handle/${newUser.handle}`
+            );
+            if (res1.data.length > 0) {
+              Alert.alert("Handle already taken");
+            } else {
+              const res2 = await axios({
                 url: `https://immense-tor-64805.herokuapp.com/api/user`,
                 method: "POST",
                 data: newUser,
               });
-              console.log(res.data);
-            } catch (err) {
-              console.error(err);
             }
           };
           makeAPICall();
