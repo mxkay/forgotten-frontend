e88b770927b340874b2a06aa5b15f4ddeff88645
Mohammad <its_mh@outlook.com>
2020-07-21 10:38:25 -0500

    nav with three routes

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Nav from "../Nav/Nav"
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function Header() {
+
+
+export default function Header({ navigation }) {
+  const Drawer = createDrawerNavigator()
+
   return (
     <View style={styles.container}>
+      {/* <Button
+        onPress={() => navigation.toggleDrawer()}
+        title="Tap"
+      /> */}
       <Text style={styles.text}>Forgotten</Text>
       <StatusBar style="auto" />
     </View>
