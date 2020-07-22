600305eff6737018252fd5d3bb7b9276a680ab87
mxkay <kayalyxvail@gmail.com>
2020-07-22 12:33:34 -0400

    Prototyped working solution

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useContext, createContext } from "react";
-import { Text, View } from "react-native";
-import UserDataContext from "./components/Shared/UserDataContext/UserDataContext";
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import UserDataContext from './components/Shared/UserDataContext/UserDataContext'
+import Header from "./components/Header/Header"
+import Footer from './components/Footer/Footer'
+import Login from './components/Login/Login'
+import Nav from "./components/Nav/Nav"
+import Home from "./components/Home/Home"
+import Profile from './components/Profile/Profile'
+import { Button, View, Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+const HeaderHome = ({navigation}) => <><Header navigation={navigation} /><Home /></>;
+const HeaderProfile = ({navigation}) => <><Header navigation={navigation} /><Profile /></>;
+
+export default function App({navigation}) {
   const [userData, setUserData] = useState({
     email: "",
     handle: "",
@@ -14,11 +22,19 @@ export default function App() {
     _id: "",
   });
   const value = { userData, setUserData };
-
+  const Drawer = createDrawerNavigator()
   return (
     <UserDataContext.Provider value={value}>
-      {userData._id ? <Text>Greetings {userData.name}</Text> : <Login />}
-      {/* <Home /> */}
+      {/* {userData._id ? */}
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home" >
+              <Drawer.Screen name="Home" component={HeaderHome} />
+              <Drawer.Screen name="Profile" component={HeaderProfile} />
+          </Drawer.Navigator> 
+        </NavigationContainer>
+      {/* :
+        <Login />
+      } */}
     </UserDataContext.Provider>
-  );
+  )
 }
