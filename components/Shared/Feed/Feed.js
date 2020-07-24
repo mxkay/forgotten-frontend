97878a0e01841a649a92ddc658a3847e1ea55f72
dychotomy <dychotomy2000@yahoo.com>
2020-07-24 11:08:49 -0400

    Fix for feed mounting

--- components/Shared/Feed/Feed.js
+++ components/Shared/Feed/Feed.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { StyleSheet, View, Dimensions } from "react-native";
 import axios from "axios";
 import Post from "../Post/Post";
@@ -7,12 +8,16 @@ const Feed = ({ lenderID, borrowerID, mode, navigation }) => {
   const [transactions, setTransactions] = useState([]);
 
   const compareTransactionDateTime = (firstTransaction, secondTransaction) => {
-    if (firstTransaction.transactionDate > secondTransaction.transactionDate) return -1;
-    else if (firstTransaction.transactionDate < secondTransaction.transactionDate) return 1;
+    if (firstTransaction.transactionDate > secondTransaction.transactionDate)
+      return -1;
+    else if (
+      firstTransaction.transactionDate < secondTransaction.transactionDate
+    )
+      return 1;
     else return 0;
-  }
+  };
 
-  useEffect(() => {
+  useFocusEffect(() => {
     const makeAPICall = async () => {
       try {
         const response = await axios(
@@ -27,29 +32,20 @@ const Feed = ({ lenderID, borrowerID, mode, navigation }) => {
   }, []);
 
   const posts = transactions
-    .filter(
-      transaction => {
-        return (
-          mode === 'strict' ?  // all queries must match
-            (!borrowerID || transaction.borrowerID == borrowerID) &&
+    .filter((transaction) => {
+      return mode === "strict" // all queries must match
+        ? (!borrowerID || transaction.borrowerID == borrowerID) &&
             (!lenderID || transaction.lenderID == lenderID)
-            :
-            (!borrowerID && !lenderID) ||
+        : (!borrowerID && !lenderID) ||
             (borrowerID && transaction.borrowerID == borrowerID) ||
-            (lenderID && transaction.lenderID == lenderID)
-        );
-      }
+            (lenderID && transaction.lenderID == lenderID);
+    })
+    .sort((firstTransaction, secondTransaction) =>
+      compareTransactionDateTime(firstTransaction, secondTransaction)
     )
-    .sort(
-      (firstTransaction, secondTransaction) => compareTransactionDateTime(firstTransaction, secondTransaction)
-    )
-    .map(
-      (transaction, index) => {
-        return (
-          <Post data={transaction} key={index} navigation={navigation} />
-        );
-      }
-    );
+    .map((transaction, index) => {
+      return <Post data={transaction} key={index} navigation={navigation} />;
+    });
 
   return <View style={styles.container}>{posts}</View>;
 };
