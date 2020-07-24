83ada3346913c9d11f170a23b3d0978fdab35ce9
mxkay <kayalyxvail@gmail.com>
2020-07-23 23:03:10 -0400

    Added SelectedPostContext.Provider

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import UserDataContext from "./components/Shared/UserDataContext/UserDataContext";
+import SelectedPostContext from "./components/Shared/SelectedPostContext/SelectedPostContext";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
@@ -19,23 +20,28 @@ export default function App() {
     name: "",
     _id: "",
   });
-  const value = { userData, setUserData };
+  const userDataValue = { userData, setUserData };
+
+  const [selectedPost, setSelectedPost] = useState('');
+  const selectedPostValue = { selectedPost, setSelectedPost };
 
   const Drawer = createDrawerNavigator();
 
   return (
-    <UserDataContext.Provider value={value}>
-      {userData._id ? (
-        <NavigationContainer>
-          <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Home" component={Home} />
-            <Drawer.Screen name="Profile" component={Profile} />
-            <Drawer.Screen name="NewPost" component={NewPost} />
-          </Drawer.Navigator>
-        </NavigationContainer>
-      ) : (
-        <Login />
-      )}
+    <UserDataContext.Provider value={userDataValue}>
+      <SelectedPostContext.Provider value={selectedPostValue}>
+        {userData._id ? (
+          <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home">
+              <Drawer.Screen name="Home" component={Home} />
+              <Drawer.Screen name="Profile" component={Profile} />
+              <Drawer.Screen name="NewPost" component={NewPost} />
+            </Drawer.Navigator>
+          </NavigationContainer>
+        ) : (
+          <Login />
+        )}
+      </SelectedPostContext.Provider>
     </UserDataContext.Provider>
   );
 }
